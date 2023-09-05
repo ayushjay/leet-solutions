1ed897f2f70ab93d7d57f4a1a2faf67d04bb943c
Ayush J <ayushtime@gmail.com>
2023-09-05 11:48:38 +0530

    added my comments on 86

--- 86 Partition List/solution.py
+++ 86 Partition List/solution.py
@@ -2,7 +2,7 @@ class Solution:
     def partition(self, head: Optional[ListNode], x: int) -> Optional[ListNode]:
         """these are dummy list nodes, NOT LINKED LISTS"""
         left,right = ListNode(), ListNode() 
-        #here we are choosing this to point 
+        #here we are choosing this to point since left, rgiht are DUMMY nodes
         ltail, rtail = left,right
 
 
