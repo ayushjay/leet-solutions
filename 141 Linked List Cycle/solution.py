631c378ba2d762fb6e36c958a04fd9f15b2ae564
Ayush J <ayushtime@gmail.com>
2023-09-09 20:16:10 +0530

    removed unrequired comments

--- 141 Linked List Cycle/solution.py
+++ 141 Linked List Cycle/solution.py
@@ -8,6 +8,11 @@ class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
         slow, fast = head, head
          #Here we write both fast and fast.next since FAST.NEXT is next iteration and FAST is moving twice as slow 
+        """
+        The condition while fast and fast.next ensures that you can safely access fast.next without encountering a NoneType error. 
+        If fast becomes None, it means that you've reached the end of the linked list, and there is no cycle.
+        Similarly, if fast.next becomes None, it means that you've reached the end of the list one step ahead of slow, which also indicates there is no cycle.
+        """
         while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
