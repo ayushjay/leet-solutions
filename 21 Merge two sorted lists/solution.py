0f27a5968322464e8657cfe70b6764fa9d836e49
Ayush J <ayushtime@gmail.com>
2023-09-09 15:18:08 +0530

    removed unrequired comments

--- 21 Merge two sorted lists/solution.py
+++ 21 Merge two sorted lists/solution.py
@@ -12,24 +12,10 @@ class Solution:
                 tail.next = list1
                 
 
-                """
-                
-
-Here's what happens step by step:
-
-Initially, list1 points to the current node in list1. Let's call this node A.
-
-When list1 = list1.next is executed, list1 is reassigned to point to the next node in the list, which is the node that follows A.
-
-list1 now points to the next node in the list, which we'll call B.
-
-This process continues as you iterate through the linked list, effectively moving list1 from one node to the next with each iteration of the loop.
-
-By moving the list1 pointer forward, you ensure that you are comparing and processing each node in list1 one by one as you traverse the linked list. 
-                """
+   
                 list1 = list1.next  # Move list1 pointer forward
             else:
-                #add the head itself and iterate head by list2.next
+                
                 tail.next = list2
                 list2 = list2.next  # Move list2 pointer forward
 
@@ -41,3 +27,4 @@ By moving the list1 pointer forward, you ensure that you are comparing and proce
             tail.next = list2
         
         return dummy.next  
+    
