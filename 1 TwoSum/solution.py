6800640dd3be519c3c43ec2e035d66cc0e67823a
Ayush J <ayushjayaswal51@gmail.com>
2024-04-15 19:34:06 +0530

    changed comment

--- 1 TwoSum/solution.py
+++ 1 TwoSum/solution.py
@@ -1,7 +1,7 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        prevMap = {} # val:index
-        for i,n in enumerate(nums):
+        prevMap = {}  # we put it like val:index
+        for i, n in enumerate(nums):
             diff = target - n
             if diff in prevMap:
                 return [prevMap[diff], i]
