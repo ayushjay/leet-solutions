15e1d6309cd942bb22c9c3ac23dc579bea51a39b
Ayush J <ayushjayaswal51@gmail.com>
2024-09-10 19:40:18 +0530

    Added comments in 242

--- 242. Valid Anagram/solution.py
+++ 242. Valid Anagram/solution.py
@@ -3,17 +3,16 @@ class Solution:
         if len(s) != len(t):
             return False
 
-        countS, countT = {},{}
+        countS, countT = {}, {}
 
         # create hashmap of both. Use python get, which returns second parameter if no
         # key found
-        # we do this to avoid error
+        # we do this to avoid error of not finding the key in first iter
         for i in range(len(s)):
-            countS[s[i]] = 1 + countS.get(s[i],0)
-            countT[t[i]] = 1 + countT.get(t[i],0)
+            countS[s[i]] = 1 + countS.get(s[i], 0)
+            countT[t[i]] = 1 + countT.get(t[i], 0)
         # to avoid error in which "c" key is not found in countT, we use python get
         for c in countS:
-            if countS[c] != countT.get(c,0):
+            if countS[c] != countT.get(c, 0):
                 return False
         return True
-        
