3310b74e106dd90b41b676ba2b7cf87ba0394d8f
Ayush J <ayushjayaswal51@gmail.com>
2024-09-11 15:23:08 +0530

    Added img for 203

--- 203. Remove Linked List Elements/solution.py
+++ 203. Remove Linked List Elements/solution.py
@@ -6,11 +6,13 @@
 
 
 """Used two pointers"""
+
+
 class Solution:
     def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
         dummy = ListNode()
         dummy.next = head
-        ##HERE, prev is dummy
+        # HERE, prev is dummy
         prev, curr = dummy, head
 
         while curr:
@@ -20,9 +22,7 @@ class Solution:
                 prev.next = nxt
             else:
                 prev = curr
-            
+
             curr = nxt
 
         return dummy.next
-
-        
