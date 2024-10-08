ce984f2ccdb8ca8e565a9ef49af30cd34dd7fd16
Ayush J <ayushjayaswal51@gmail.com>
2024-10-08 16:24:36 +0530

    made changes to Deque.py

--- 0. Common DSA/queue/Deque.py
+++ 0. Common DSA/queue/Deque.py
@@ -1,4 +1,3 @@
-
 from collections import deque
 
 customQueue = deque(maxlen=3)
@@ -8,6 +7,8 @@ customQueue.append(1)
 customQueue.append(2)
 customQueue.append(3)
 customQueue.append(4)
+print(customQueue)
+print(customQueue.popleft())
 print(customQueue)
 print(customQueue.clear())
 print(customQueue)
