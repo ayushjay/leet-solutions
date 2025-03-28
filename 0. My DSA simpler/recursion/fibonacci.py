a32a849424aabd02cff4168c61d2f17d3f633941
Ayush J <ayushjayaswal51@gmail.com>
2025-03-28 20:07:32 +0530

    added call stack image

--- 0. My DSA simpler/recursion/fibonacci.py
+++ 0. My DSA simpler/recursion/fibonacci.py
@@ -4,5 +4,24 @@ def fibonacci(n):
     if n <= 1:
         return n
 
-    # Recursive case: fib(n) = fib(n - 1) + fib(n - 1)
+    # Recursive case: fib(n) = fib(n - 1) + fib(n - 2)
     return fibonacci(n - 1) + fibonacci(n - 2)
+
+
+print(fibonacci(7))
+# 1,1,2,3,5,8,13
+"""
+fibonacci(7)
+├── fibonacci(6)
+│   ├── fibonacci(5)
+│   │   ├── fibonacci(4)
+│   │   │   ├── fibonacci(3)
+│   │   │   │   ├── fibonacci(2)
+│   │   │   │   │   ├── fibonacci(1) → 1
+│   │   │   │   │   ├── fibonacci(0) → 0
+│   │   │   │   ├── fibonacci(2) → 1
+│   │   │   ├── fibonacci(3) → 2
+│   │   ├── fibonacci(4) → 3
+│   ├── fibonacci(5) → 5
+├── fibonacci(6) → 8
+"""
