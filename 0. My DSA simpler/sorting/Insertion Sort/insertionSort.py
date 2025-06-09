4dfd1fd38c396b188f8e0c8f9f5f0d72c1cebb3f
Ayush J <ayushjayaswal51@gmail.com>
2025-06-09 15:15:19 +0530

    comment changes

--- 0. My DSA simpler/sorting/Insertion Sort/insertionSort.py
+++ 0. My DSA simpler/sorting/Insertion Sort/insertionSort.py
@@ -3,11 +3,12 @@
 
 def insertionSort(arr):
 	# Traverse through 1 to len(arr)
+    # Start at 1st Index
     for i in range(1, len(arr)):
         j = i - 1
         while j >= 0 and arr[j + 1] < arr[j]:
             # arr[j] and arr[j + 1] are out of order so swap them 
-            # j <= 0 keeps it in bounce, no index error
+            # j >= 0 keeps it in bounce, no index error
             #  SWAP
             tmp = arr[j + 1]
             arr[j + 1] = arr[j]
